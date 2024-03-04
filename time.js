4108f71b86ecb6075523bb27ec2bfb4ffc09310d
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2024-03-04 23:03:10 +0530

    added timestamps and date

---
+++ time.js
@@ -0,0 +1,25 @@
+function pad(number) {
+    return (number < 10 ? '0' : '') + number;
+}
+
+function generateTimeRadios() {
+    var timeRadiosDiv = document.getElementById("timestamps");
+    for (var hour = 9; hour <= 19; hour++) {
+        var timeString = pad(hour % 12) + ":" + pad(0) + (hour < 12 ? "AM" : "PM");
+        var epochValue = (hour * 60 + 0) * 60;
+        var radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = "timestamp";
+        radio.value = timeString;
+        radio.id = "timeRadio" + epochValue;
+        var label = document.createElement("label");
+        label.htmlFor = "timeRadio" + epochValue;
+        label.appendChild(document.createTextNode(timeString));
+        const wrapper = document.createElement("div");
+        wrapper.className = "wrapper";
+        wrapper.append(radio, label);
+        timeRadiosDiv.appendChild(wrapper);
+    }
+}
+
+window.onload = generateTimeRadios;
