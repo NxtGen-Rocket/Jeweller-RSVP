79ccbb1871a65b503ad9d0a2fac9d7572f558e53
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2024-03-04 23:42:12 +0530

    timings changed

--- time.js
+++ time.js
@@ -1,11 +1,14 @@
+
 function pad(number) {
+    if (number == 0) return 12;
     return (number < 10 ? '0' : '') + number;
 }
 
+
 function generateTimeRadios() {
     var timeRadiosDiv = document.getElementById("timestamps");
-    for (var hour = 9; hour <= 19; hour++) {
-        var timeString = pad(hour % 12) + ":" + pad(0) + (hour < 12 ? "AM" : "PM");
+    for (var hour = 11; hour <= 19; hour++) {
+        var timeString = pad(hour % 12) + ":00" + (hour < 12 ? "AM" : "PM");
         var epochValue = (hour * 60 + 0) * 60;
         var radio = document.createElement("input");
         radio.type = "radio";
